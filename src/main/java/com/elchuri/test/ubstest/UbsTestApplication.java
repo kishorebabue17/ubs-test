e6f16c6f2b3bde6e1cd9611b965df91f706371c0
kishore babu elchuri <kishore.elchuri@gmail.com>
2021-06-16 20:55:45 -0400

    first commit

---
+++ src/main/java/com/elchuri/test/ubstest/UbsTestApplication.java
@@ -0,0 +1,11 @@
+package com.elchuri.test.ubstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UbsTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/elchuri/test/ubstest/UbsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elchuri.test.ubstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
